2eaf1074d866255737c02eba6fabebcce976ea3b
analogy47 <actweed47@gmail.com>
2018-06-11 14:30:03 -0400

    Added react app set-up

---
+++ hairspray-app/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">WIP</div>;
+  }
+}
+
+export default App;
