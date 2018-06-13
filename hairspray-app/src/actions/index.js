4fde40c306c6976c2e4db2bafc5fc6d29c578eb6
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-06-13 17:30:56 -0400

    UserSetttings final changes. should be able to change user settings

--- hairspray-app/src/actions/index.js
+++ hairspray-app/src/actions/index.js
@@ -1,23 +1,31 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3000';
+const URL = 'http://localhost:5000';
 // Todo: make actions and variables for all actions
+export const TOGGLE_UPDATE_USER_FORM = "TOGGLE_UPDATE_USER_FORM";
 export const USER_UPDATING = "USER_UPDATING";
 export const USER_UPDATE_COMPLETE = "USER_UPDATE_COMPLETE";
 export const ERROR = "ERROR";
 
+export const toggleUpdateForm = () => {
+    return {
+      type: TOGGLE_UPDATE_USER_FORM
+    };
+  };
+
 // Change user settings
 export const userSettingsChange = updates => {
-    const { id, name, number, email } = updates;
+    const { id, name, number, email, password } = updates;
     return dispatch => {
-        dispatch({ USER_UPDATING });
+        dispatch({ type: USER_UPDATING });
         axios
-            .put(`${URL}/users/${id}`, {name, number, email })
+            .put(`${URL}/users/${id}`, {name, number, email, password })
             .then(updatedUser => {
-                dispatch({ type: USER_UPDATE_COMPLETE, payload: updatedUser});
+                console.log("ACTIONs: ", updatedUser.data);
+                dispatch({ type: USER_UPDATE_COMPLETE, payload: updatedUser.data});
             })
             .catch(err => {
-                dispatch(err.toString());
+                dispatch({ type: err });
             });
     }
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,14 +1,14 @@
-const userControllerStrategy = require("../controllers/userController");
-const express = require("express");
-const middleAuth = require("../middleware/middleware");
+const userControllerStrategy = require('../controllers/userController');
+const express = require('express');
+const middleAuth = require('../middleware/middleware');
 
-module.exports = app => {
+module.exports = (app) => {
   app
-    .route("/create-user")
+    .route('/create-user')
     .post(middleAuth.hashedPassword, userControllerStrategy.createUser);
-  app.route("/users/:id").get(userControllerStrategy.getUser);
-  app.route("/users/:id").put(userControllerStrategy.updateUser);
+  app.route('/users/:id').get(userControllerStrategy.getUser);
+  app.route('/users/:id').put(userControllerStrategy.updateUser);
   app
-    .route("/login")
+    .route('/login')
     .post(middleAuth.authenticate, userControllerStrategy.userLogin);
 };
