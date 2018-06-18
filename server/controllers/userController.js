5950cb7c4c36e0b485f04458e81cbf102e651257
Seth Anthony <steven.magadan@hotmail.com>
2018-06-18 16:45:04 -0400

    new charge changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ var settings = require("../config/settings");
 var jwt = require("jsonwebtoken");
 const { requireAuth, getTokenForUser } = require("../config/auth");
 const bcrypt = require("bcrypt");
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const stripe = require("stripe")("sk_test_vY2PFCv47VGRTiS3Cb9c7uky");
 const keyPublish = process.env.PUBLISHABLE_KEY;
 
 User.create({
@@ -95,21 +95,31 @@ const updateUser = (req, res) => {
   });
 };
 
+const createCustomer = (req, res) => {
+  const { email, cart } = req.body;
+  const token = req.body.stripeToken;
+  const makeCustomer = stripe.customers.create({
+    email: email,
+    source: token
+  });
+  makeCustomer
+    .then(createdCustomer => res.json(createCustomer))
+    .catch(err => res.json(err));
+};
+
 const createCharge = (req, res) => {
   const token = req.body.stripeToken;
-  const { email, cart } = req.body;
-  const makeCharge = stripe.charges.create(
-    {
-      amount: 1200,
-      currency: "usd",
-      description: "This is a sample charge",
-      customer: email,
-      source: token
-    },
-    makeCharge
-      .then(createdCharge => res.json(createdCharge))
-      .catch(err => res.json(err))
-  );
+  const { email } = req.body;
+  const makeCharge = stripe.charges.create({
+    amount: 1200,
+    currency: "usd",
+    description: "This is a sample charge",
+    customer: email,
+    source: token
+  });
+  makeCharge
+    .then(createdCharge => res.json(createdCharge))
+    .catch(err => res.json(err));
 };
 
 module.exports = {
