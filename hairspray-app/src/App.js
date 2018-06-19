5292ea49a1fc55450a47ea29fd4f0af4af6890f8
Seth Anthony <steven.magadan@hotmail.com>
2018-06-19 16:47:13 -0400

    halfway done with stripeForm

--- hairspray-app/src/App.js
+++ hairspray-app/src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import UserSettings from './components/UserSettings.js';
-import './App.css';
-import LandPage from './components/LandPage';
+import React, { Component } from "react";
+import UserSettings from "./components/UserSettings.js";
+import "./App.css";
+import LandPage from "./components/LandPage";
+import { StripeProvider } from "react-stripe-elements";
+
 // import Header from './components/Header';
 
 class App extends Component {
   user = {
-    id: '5b216c18fa2b7528d03db8b5',
-    name: 'test',
-    email: 'testEmail@test.com',
-    number: '1234567890',
-    password: 'testPassword',
+    id: "5b216c18fa2b7528d03db8b5",
+    name: "test",
+    email: "testEmail@test.com",
+    number: "1234567890",
+    password: "testPassword"
   };
 
   render() {
