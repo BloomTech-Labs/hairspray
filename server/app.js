2734eeed2a1852d3fc292ba3d7a9b14d41dfed40
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-06-22 12:53:33 -0400

    heroku changes for mongo

--- server/app.js
+++ server/app.js
@@ -13,7 +13,9 @@ const CORS = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
-const db = "mongodb://localhost:27017/hairspray";
+const db =
+	"mongodb://localhost:27017/hairspray" ||
+	"mongodb://Neardanger:hairspray123@ds117061.mlab.com:17061/hairspray";
 const routes = require("./routes/routes");
 
 app.use(express.static(path.join(__dirname, "../hairspray-app/build")));
@@ -21,9 +23,9 @@ app.use(express.static(path.join(__dirname, "../hairspray-app/build")));
 // connect to database
 
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB is connected"))
-  .catch(err => console.log(err));
+	.connect(db)
+	.then(() => console.log("MongoDB is connected"))
+	.catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -32,24 +34,24 @@ app.use(CORS());
 //const REACT_WHITELIST = require("../hairspray-app/src/components/StripeCheckout");
 
 const whitelist = [
-  "http://localhost:5000",
-  //REACT_WHITELIST,
-  "https://obscure-island-58835.herokuapp.com/"
+	"http://localhost:5000",
+	//REACT_WHITELIST,
+	"https://obscure-island-58835.herokuapp.com/"
 ];
 
 app.use(
-  CORS({
-    origin: whitelist,
-    credentials: true,
-    methods: ["GET", "PUT", "POST", "DELETE"]
-  })
+	CORS({
+		origin: whitelist,
+		credentials: true,
+		methods: ["GET", "PUT", "POST", "DELETE"]
+	})
 );
 app.options("*", CORS());
 
 routes(app);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(`${__dirname  }/hairspray-app/build/index.html`));
+	res.sendFile(path.join(`${__dirname}/hairspray-app/build/index.html`));
 });
 
 app.listen(port, () => console.log(`app running on port ${port}`));
