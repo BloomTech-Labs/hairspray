46373474ab44492e9e43b9bcbcdab09af13753cb
Seth Anthony <friendlierchemist@gmail.com>
2018-06-28 22:04:01 -0400

    working on reminder texts

--- hairspray-app/src/components/SignUp.js
+++ hairspray-app/src/components/SignUp.js
@@ -3,66 +3,68 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { reduxForm, Field } from "redux-form";
 import { register } from "../actions";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+
 class SignUp extends Component {
-	handleFormSubmit = values => {
-		this.props.register(values, this.props.history);
-	};
+  handleFormSubmit = values => {
+    this.props.register(values, this.props.history);
+  };
 
-	renderAlert = () => {
-		if (!this.props.error) return null;
-		return <h3>{this.props.error}</h3>;
-	};
+  renderAlert = () => {
+    if (!this.props.error) return null;
+    return <h3>{this.props.error}</h3>;
+  };
 
-	render() {
-		const { handleSubmit } = this.props;
+  render() {
+    const { handleSubmit } = this.props;
 
-		return (
-			<div>
-				<div>
-					<Link to="/signin">Sign In</Link>
-				</div>
-				<div className="signup-form">
-					<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-						<fieldset>
-							<label>Name</label>
-							<Field name="name" component="input" type="text" />
-						</fieldset>
-						<fieldset>
-							<label>Phone</label>
-							<Field name="phone" component="input" type="text" />
-						</fieldset>
-						<fieldset>
-							<label>Email</label>
-							<Field name="email" component="input" type="email" />
-						</fieldset>
-						<fieldset>
-							<label>Password</label>
-							<Field name="password" component="input" type="password" />
-						</fieldset>
-						<fieldset>
-							<label>Confirm Password</label>
-							<Field name="confirmPassword" component="input" type="password" />
-						</fieldset>
-						<button action="submit">Sign Up</button>
-						{this.renderAlert()}
-					</form>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <div>
+          <Link to="/signin">Sign In</Link>
+        </div>
+        <div className="signup-form">
+          <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <fieldset>
+              <label>Name</label>
+              <Field name="name" component="input" type="text" />
+            </fieldset>
+            <fieldset>
+              <label>Phone</label>
+              <Field name="phone" component="input" type="text" />
+            </fieldset>
+            <fieldset>
+              <label>Email</label>
+              <Field name="email" component="input" type="email" />
+            </fieldset>
+            <fieldset>
+              <label>Password</label>
+              <Field name="password" component="input" type="password" />
+            </fieldset>
+            <fieldset>
+              <label>Confirm Password</label>
+              <Field name="confirmPassword" component="input" type="password" />
+            </fieldset>
+            <button action="submit">Sign Up</button>
+            {this.renderAlert()}
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => {
-	return {
-		error: state.auth.error
-	};
+  return {
+    error: state.auth.error
+  };
 };
 
 SignUp = connect(
-	mapStateToProps,
-	{ register }
+  mapStateToProps,
+  { register }
 )(SignUp);
 
 export default reduxForm({
-	form: "signup",
-	fields: ["name", "phone", "email", "password", "confirmPassword"]
+  form: "signup",
+  fields: ["name", "phone", "email", "password", "confirmPassword"]
 })(SignUp);
