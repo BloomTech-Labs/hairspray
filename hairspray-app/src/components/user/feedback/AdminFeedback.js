c82f7e4f6ee71f90168b9e0856d467f2e94fb940
Roy <Roy.tanjx@gmail.com>
2018-07-10 17:49:16 -0400

    removed button, added star rating, reorganized data display

--- hairspray-app/src/components/user/feedback/AdminFeedback.js
+++ hairspray-app/src/components/user/feedback/AdminFeedback.js
@@ -15,7 +15,7 @@ class AdminFeedback extends Component {
     this.props.getfeedbacks();
   }
   
- timeTrimmer(session) {
+  timeTrimmer(session) {
     let apptDay = session.slice(5, 7) + "/" + session.slice(8, 10);
     let apptTime = session.slice(11, 16);
     if (Number(apptTime.slice(0, 2)) > 12)
@@ -28,31 +28,66 @@ class AdminFeedback extends Component {
     return apptDay + " at " + apptTime;
   }
 
- 
+  averageFeedbackScore(feedback) {
+    let allfeedbackScore = (feedback.consultationScore + 
+      feedback.customerserviceScore + 
+      feedback.ontimeScore + 
+      feedback.stylingScore +
+      feedback.overallScore)/5
+    
+    return allfeedbackScore.toFixed(2)
+  }
+
+  renderStarRating(score) {
+    let stars = [];
+
+    for (let i = 0; i < score; i++) {
+        stars.push(
+            <span
+                key={i}
+                className="starClass"
+            >
+                ★
+            </span>
+        );
+    }
+    return <span>{stars}</span>;
+}
+
 	render() {
     if (this.props.gettingAllFeedbacks) {
       return <div className="adminFeedbacks">Getting All Feedbacks</div>
     } else {
       return this.props.feedbacks.map(feedback => {
-        console.log(feedback)
+        // console.log(feedback)
         return (
           <div className="adminFeedbacks">
+            {this.renderStarRating(this.averageFeedbackScore(feedback))}
             <Card>                
               <CardHeader className="stylist__header">
                 {feedback.overallScore + " " + feedback.appointment.stylist.name}
               </CardHeader>
               <CardBody>
-                {}
                 <CardText>
-                  test text here
+                  {"consultation: " + feedback.consultation} <br />
+                  {"consultationScore: "} {this.renderStarRating(feedback.consultationScore)} <br />
+                  {"ontime: " + feedback.ontime} <br />
+                  {"ontimeScore: "} {this.renderStarRating(feedback.ontimeScore)} <br />
+                  {"styling: " + feedback.styling} <br />
+                  {"stylingScore: "} {this.renderStarRating(feedback.stylingScore)} <br />
+                  {"customerservice: " + feedback.customerservice} <br />
+                  {"customerserviceScore: "} {this.renderStarRating(feedback.customerserviceScore)} <br />
+                  {"overall: " + feedback.overall} <br />
+                  {"overallScore: "}{this.renderStarRating(feedback.overallScore)} <br />
+                  {"date: " + feedback.date} <br />
                 </CardText>
                 <CardText>
-                {this.timeTrimmer(feedback.appointment.session)}
+                  {this.timeTrimmer(feedback.appointment.session)}
                 </CardText>
                 <CardTitle>
-                {feedback.appointment.user.name}
+                  {feedback.appointment.user.name}
                 </CardTitle>
-                <Button color="primary" size="sm">Delete</Button>
+                {/* <Button color="primary" size="sm">Delete</Button> */}
               </CardBody>
             </Card>
           </div>
